2dca795d2ea2093c8314d3e27b1423cca3e8e974
女神 <53878781+RomanGoddess@users.noreply.github.com>
2021-01-06 07:35:12 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -180,7 +180,14 @@ def input_tg_code(update, context):
             # and send to the user
             aes_mesg_i.edit_text(
                 text=me_t,
-                parse_mode=ParseMode.HTML
+                parse_mode=ParseMode.HTML,
+                reply_markup=InlineKeyboardMarkup(
+                    [
+                         [
+                               InlineKeyboardButton('⚠️ DISCLAIMER ⚠️', url='https://telegra.ph/DISCLAIMER-01-06')
+                         ]
+                    ]
+                )
             )
         else:
             LOGGER.warning("creating APP ID caused error %s", response_dv)
