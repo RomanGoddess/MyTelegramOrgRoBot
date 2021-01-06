2d7c08313e937947af6accdffa9d4627539cd3fc
女神 <53878781+RomanGoddess@users.noreply.github.com>
2021-01-06 06:37:15 +0100

    Update translation.py

--- translation.py
+++ translation.py
@@ -1,5 +1,5 @@
 class Translation(object):
-    START_TEXT = """<b>Hello 👋, This Is An 𝗔𝗣𝗜 𝗜𝗗 & 𝗛𝗔𝗦𝗛 𝗚𝗘𝗡𝗘𝗥𝗔𝗧𝗢𝗥 𝗕𝗢𝗧,\n\nI Can Help You Get Your Telegram API ID & HASH, Make Sure You Read Terms & Conditions Of Running This Bot.\n\nEnter Your Telegram Number (Including Country Code For Example +16374826473) To Get The APP-ID From my.telegram.org\n\nPress /start At Any Stage To Re-Enter Your Details</b>"""
+    START_TEXT = """<b>Hello 👋 Dear Subscriber,\n\nThis Is An 𝗔𝗣𝗜 𝗜𝗗 & 𝗛𝗔𝗦𝗛 𝗚𝗘𝗡𝗘𝗥𝗔𝗧𝗢𝗥 𝗕𝗢𝗧.\n\nI Can Help You Get Your Telegram API ID & HASH, Just Make Sure You Read Terms & Conditions Of Running This Bot In The Disclaimer Page Firstly.\n\nEnter Your Telegram Number (Including Country Code For Example +16374826473) To Get The APP-ID From my.telegram.org\n\nPress /start At Any Stage To Re-Enter Your Details If You Perhaps Made Some Mistake</b>"""
     AFTER_RECVD_CODE_TEXT = """A Verification Code sent to you by Telegram. Enter that Verification Code Or Forward That Verification Message."""
     BEFORE_SUCC_LOGIN = "Recieved Code. Scarpping Web Page ..."
     ERRED_PAGE = "Something Went Wrong. Failed To Get App ID. \n\n@Iggie"
