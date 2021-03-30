3c26047f03611b58e1569b7af3e513f0dee40aee
女神 <53878781+RomanGoddess@users.noreply.github.com>
2021-03-30 14:00:55 +0100

    Update and rename config.py to sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -6,7 +6,7 @@ class Config(object):
     TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", None)
     # required for running on Heroku
     URL = os.environ.get("URL", "")
-    PORT = int(os.environ.get("PORT", None))
+    PORT = int(os.environ.get("PORT", 5000))
     # Python3 ReQuests CHUNK SIZE
     CHUNK_SIZE = 10280
     # MyTelegram.org
